388b7fd406c53c2cb4c1e27578c00dc0a2cf4ad5
Day4n1ght <106701507+Day4n1ght@users.noreply.github.com>
2026-01-02 20:36:01 +0800

    Update others.js

--- src/data/courses/others.js
+++ src/data/courses/others.js
@@ -8,9 +8,15 @@ export const OTHER_COURSES = {
     fullName: "Kelab Golf Putra",
     location: ["Kangar", "Perlis", "Malaysia"],
     pars: [5,4,4,3,4,5,4,3,4, 4,5,3,4,4,4,3,4,5],
-    blueTees: null,
-    whiteTees: null,
-    redTees: null
+    index: [5,7,11,17,13,1,15,9,3, 8,12,18,14,4,10,16,2,6],
+  },
+
+  "PUTRA_GOLF_CLUB": {
+    shortName: "PGC",
+    fullName: "Putra Golf Club",
+    location: ["Kangar", "Perlis", "Malaysia"],
+    pars: [5,4,4,3,4,5,4,3,4, 4,5,3,4,4,4,3,4,5],
+    index: [5,7,11,17,13,1,15,9,3, 8,12,18,14,4,10,16,2,6],
   },
 
   // === Labuan ===
@@ -19,9 +25,7 @@ export const OTHER_COURSES = {
     fullName: "Labuan International Golf Club",
     location: ["Labuan", "Labuan", "Malaysia"],
     pars: [4,5,5,4,3,4,3,4,4, 5,4,4,3,5,4,3,4,4],
-    blueTees: null,
-    whiteTees: null,
-    redTees: null
+    index: [9,3,1,13,17,11,15,5,7, 4,10,6,16,2,8,18,14,12],
   }
 };
 
